adab12b484babcdd4b7ca379d67ec01613bc3e34
Frederik B Eriksen <frederik.byskov.erisen@gmail.com>
2024-11-24 19:21:13 +0100

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <section>
+      <div className="container">
+        <h1 className="text-3xl font-bold">Next TS starter</h1>
+      </div>
+    </section>
+  )
+}
